7ec0ee3b6a181e1b2ebd024658e7dca1a87c6c54
demns <demn_@tut.by>
2019-08-05 21:56:52 +0300

    Add possible move service template

--- CheckersBot/Services/MoveService.cs
+++ CheckersBot/Services/MoveService.cs
@@ -8,6 +8,25 @@ namespace CheckersBot.Services
     {
         public List<Move> GetNextMove(BoardModel currentBoard)
         {
+            var beats = GetPossibleBeats(currentBoard);
+
+            if (beats.Count > 0)
+            {
+                return beats[0];
+            }
+            else
+            {
+                return GetPossibleMoves(currentBoard)[0];
+            }
+
+        }
+        public List<List<Move>> GetPossibleBeats(BoardModel currentBoard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<List<Move>> GetPossibleMoves(BoardModel currentBoard)
+        {
             throw new NotImplementedException();
         }
     }
