9b1508b28d91dff930afc2505866b0ee85ed24ed
demns <demn_@tut.by>
2019-08-07 09:36:54 +0300

    Adds possibility for getPossibleBeats to beat more than once

--- CheckersBot/Game/PossibleBeatsCalc.cs
+++ CheckersBot/Game/PossibleBeatsCalc.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using CheckersBot.Extensions;
 using CheckersBot.Models;
 
 namespace CheckersBot.Game
@@ -6,8 +8,8 @@ namespace CheckersBot.Game
     public class PossibleBeatsCalc : IPossibleBeatsCalc
     {
         public List<List<Move>> GetPossibleBeats(CellState[,] boardArray, Team teamPlaying)
-        { 
-            var possibleMoves = new List<List<Move>>();
+        {
+            var possibleBeats = new List<List<Move>>();
 
             for (int i = 0; i < 8; i++)
             {
@@ -18,25 +20,29 @@ namespace CheckersBot.Game
                         if (i - 2 >= 0 && j - 2 >= 0 &&
                             boardArray[i - 1, j - 1] == CellState.BlackPiece && boardArray[i - 2, j - 2] == CellState.Empty)
                         {
-                            possibleMoves.Add(new List<Move>
+                            var beats = new List<Move>
                             {
                                 new Move
                                 {
                                     StartingPoint = new Cell { X = i, Y = j },
                                     EndingPoint = new Cell { X = i - 2, Y = j - 2 }
                                 }
-                            });
+                            };
+                            appendSubsequentBeats(Clone.CloneJson(boardArray), teamPlaying, beats);
+                            possibleBeats.Add(beats);
                         }
                         if (i + 2 <= 7 && j - 2 >= 0 &&
                             boardArray[i + 1, j - 1] == CellState.BlackPiece && boardArray[i + 2, j - 2] == CellState.Empty)
                         {
-                            possibleMoves.Add(new List<Move>
+                            var beats = new List<Move>
                             {
                                 new Move {
                                     StartingPoint = new Cell { X = i, Y = j },
                                     EndingPoint = new Cell { X = i + 2, Y = j - 2 }
                                 }
-                            });
+                            };
+                            appendSubsequentBeats(Clone.CloneJson(boardArray), teamPlaying, beats);
+                            possibleBeats.Add(beats);
                         }
                     }
                     else if (boardArray[i, j] == CellState.BlackPiece && teamPlaying == Team.Black)
@@ -44,32 +50,137 @@ namespace CheckersBot.Game
                         if (i + 2 <= 7 && j + 2 <= 7 &&
                             boardArray[i + 1, j + 1] == CellState.WhitePiece && boardArray[i + 2, j + 2] == CellState.Empty)
                         {
-                            possibleMoves.Add(new List<Move>
+                            var beats = new List<Move>
                             {
                                 new Move
                                 {
                                     StartingPoint = new Cell { X = i, Y = j },
                                     EndingPoint = new Cell { X = i + 2, Y = j + 2 }
                                 }
-                            });
+                            };
+
+                            appendSubsequentBeats(Clone.CloneJson(boardArray), teamPlaying, beats);
+                            possibleBeats.Add(beats);
                         }
                         if (i - 2 >= 0 && j + 2 <= 7 &&
                             boardArray[i - 1, j + 1] == CellState.WhitePiece && boardArray[i - 2, j + 2] == CellState.Empty)
                         {
-                            possibleMoves.Add(new List<Move>
+                            var beats = new List<Move>
                             {
                                 new Move
                                 {
                                     StartingPoint = new Cell { X = i, Y = j },
                                     EndingPoint = new Cell { X = i - 2, Y = j + 2 }
                                 }
-                            });
+                            };
+
+                            appendSubsequentBeats(Clone.CloneJson(boardArray), teamPlaying, beats);
+                            possibleBeats.Add(beats);
                         }
                     }
                 }
             }
 
-            return possibleMoves;
+            return possibleBeats;
+        }
+
+        private void appendSubsequentBeats(CellState[,] boardArray, Team teamPlaying, List<Move> beats)
+        {
+            var beat = beats[beats.Count - 1];
+            var checker = boardArray[beat.StartingPoint.X, beat.StartingPoint.Y];
+            boardArray[beat.StartingPoint.X, beat.StartingPoint.Y] = CellState.Empty;
+            boardArray[(beat.StartingPoint.X + beat.EndingPoint.X) / 2, (beat.StartingPoint.Y + beat.EndingPoint.Y) / 2] = 
+                CellState.Empty;
+            boardArray[beat.EndingPoint.Y, beat.EndingPoint.Y] = checker;
+
+            var i = beat.EndingPoint.X;
+            var j = beat.EndingPoint.Y;
+            if (teamPlaying == Team.White)
+            {
+                if (i - 2 >= 0 && j - 2 >= 0 &&
+                    boardArray[i - 1, j - 1] == CellState.BlackPiece && boardArray[i - 2, j - 2] == CellState.Empty)
+                {
+                    beats.Add(new Move
+                    {
+                        StartingPoint = new Cell { X = i, Y = j },
+                        EndingPoint = new Cell { X = i - 2, Y = j - 2 }
+                    });
+                    appendSubsequentBeats(boardArray, teamPlaying, beats);
+                }
+                else if (i + 2 <= 7 && j - 2 >= 0 &&
+                  boardArray[i + 1, j - 1] == CellState.BlackPiece && boardArray[i + 2, j - 2] == CellState.Empty)
+                {
+                    beats.Add(new Move
+                    {
+                        StartingPoint = new Cell { X = i, Y = j },
+                        EndingPoint = new Cell { X = i + 2, Y = j - 2 }
+                    });
+                    appendSubsequentBeats(boardArray, teamPlaying, beats);
+                }
+                else if (i + 2 <= 7 && j + 2 <= 7 &&
+                  boardArray[i + 1, j + 1] == CellState.BlackPiece && boardArray[i + 2, j + 2] == CellState.Empty)
+                {
+                    beats.Add(new Move
+                    {
+                        StartingPoint = new Cell { X = i, Y = j },
+                        EndingPoint = new Cell { X = i + 2, Y = j + 2 }
+                    });
+                    appendSubsequentBeats(boardArray, teamPlaying, beats);
+                }
+                else if (i - 2 >= 0 && j + 2 <= 7 &&
+                  boardArray[i - 1, j + 1] == CellState.BlackPiece && boardArray[i - 2, j + 2] == CellState.Empty)
+                {
+                    beats.Add(new Move
+                    {
+                        StartingPoint = new Cell { X = i, Y = j },
+                        EndingPoint = new Cell { X = i - 2, Y = j + 2 }
+                    });
+                    appendSubsequentBeats(boardArray, teamPlaying, beats);
+                }
+            }
+            else if (teamPlaying == Team.Black)
+            {
+                if (i + 2 <= 7 && j + 2 <= 7 &&
+                    boardArray[i + 1, j + 1] == CellState.WhitePiece && boardArray[i + 2, j + 2] == CellState.Empty)
+                {
+                    beats.Add(new Move
+                    {
+                        StartingPoint = new Cell { X = i, Y = j },
+                        EndingPoint = new Cell { X = i + 2, Y = j + 2 }
+                    });
+                    appendSubsequentBeats(boardArray, teamPlaying, beats);
+                }
+                else if (i - 2 >= 0 && j + 2 <= 7 &&
+                  boardArray[i - 1, j + 1] == CellState.WhitePiece && boardArray[i - 2, j + 2] == CellState.Empty)
+                {
+                    beats.Add(new Move
+                    {
+                        StartingPoint = new Cell { X = i, Y = j },
+                        EndingPoint = new Cell { X = i - 2, Y = j + 2 }
+                    });
+                    appendSubsequentBeats(boardArray, teamPlaying, beats);
+                }
+                else if (i - 2 >= 0 && j - 2 >= 0 &&
+                  boardArray[i - 1, j - 1] == CellState.WhitePiece && boardArray[i - 2, j - 2] == CellState.Empty)
+                {
+                    beats.Add(new Move
+                    {
+                        StartingPoint = new Cell { X = i, Y = j },
+                        EndingPoint = new Cell { X = i - 2, Y = j - 2 }
+                    });
+                    appendSubsequentBeats(boardArray, teamPlaying, beats);
+                }
+                else if (i + 2 <= 7 && j - 2 >= 0 &&
+                  boardArray[i + 1, j - 1] == CellState.WhitePiece && boardArray[i + 2, j - 2] == CellState.Empty)
+                {
+                    beats.Add(new Move
+                    {
+                        StartingPoint = new Cell { X = i, Y = j },
+                        EndingPoint = new Cell { X = i + 2, Y = j - 2 }
+                    });
+                    appendSubsequentBeats(boardArray, teamPlaying, beats);
+                }
+            }
         }
     }
 }
